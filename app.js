30a7415088ae5cba42718fcbdf7f7687036808c1
toraraffi-maker <toraraffi@gmail.com>
2026-09-04 20:56:08 +0700

    semangat

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const apel=5.000;
+const pisang=10.000;
+const diskon=0.1;
+const apel3=5.000+5.000+5.000;
+const pisang2=10.00+10.000;
+const hitungAwal=apel3+pisang2;
+const hitung=hitungAwal-hitungAwal*diskon;
+console.log(hitung);
